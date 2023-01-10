6cce6831a7124d038052a8907917815dd034ce4d
Milos Milosavljevic <milosh@emisia.net>
2023-01-10 20:42:51 +0100

    Testing helper

--- pkg/utils/testing.go
+++ pkg/utils/testing.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"github.com/golang/mock/gomock"
+	"io"
 	"reflect"
 	"runtime/debug"
 	"testing"
@@ -26,4 +27,19 @@ func (e errMatcher) Matches(x interface{}) bool {
 
 func (e errMatcher) String() string {
 	return fmt.Sprintf("is equal to %v", e.x)
+}
+
+type NopWriteCloser struct {
+	io.Writer
+}
+
+func NewNopWriteCloser(writer io.Writer) *NopWriteCloser {
+	return &NopWriteCloser{
+		Writer: writer,
+	}
+}
+
+func (*NopWriteCloser) Close() error {
+	// Noop
+	return nil
 }
